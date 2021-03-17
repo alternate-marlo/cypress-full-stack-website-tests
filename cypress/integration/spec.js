b4c7b1a336756aff23b1aa70c1c6a9a1ab9d31e5
Marlo Vinall Richardson <marlovinallrichardson@gmail.com>
2021-03-18 10:54:31 +1100

    * Add Qase reporting integration

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,18 +1,27 @@
 /// <reference types="cypress" />
+
+import { qase } from 'cypress-qase-reporter/dist/mocha';
+
 describe('Gatsby blog', () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit('/');
+  });
 
-  it('loads home page', () => {
-    cy.get('main > div > h1')
-  })
+  qase(
+    1,
+    it('loads home page', () => {
+      cy.get('main > div > h1');
+    })
+  );
 
-  it('navigates to second page and back', () => {
-    cy.contains('a', 'Go to page 2').click()
-    cy.url().should('match', /page-2/)
-    cy.contains('main > h1', 'Hi from the second page')
-    cy.contains('a', 'Go back to the homepage').click()
-    cy.url().should('include', Cypress.config('baseUrl'))
-  })
-})
+  qase(
+    [2, 3],
+    it('navigates to second page and back', () => {
+      cy.contains('a', 'Go to page 2').click();
+      cy.url().should('match', /page-2/);
+      cy.contains('main > h1', 'Hi from the second page');
+      cy.contains('a', 'Go back to the homepage').click();
+      cy.url().should('include', Cypress.config('baseUrl'));
+    })
+  );
+});
