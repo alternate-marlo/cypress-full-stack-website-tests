3d26de1d7a34820aafb2f0c1836d2a344652ffb9
Marlo Vinall Richardson <marlovinallrichardson@gmail.com>
2021-03-18 21:00:21 +1100

    * Remove Qase integration

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,27 +1,19 @@
 /// <reference types="cypress" />
 
-import { qase } from 'cypress-qase-reporter/dist/mocha';
-
 describe('Gatsby blog', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
-  qase(
-    1,
-    it('loads home page', () => {
-      cy.get('main > div > h2');
-    })
-  );
+  it('loads home page', () => {
+    cy.get('main > div > h2');
+  });
 
-  qase(
-    [2, 3],
-    it('navigates to second page and back', () => {
-      cy.contains('a', 'Go to page 2').click();
-      cy.url().should('match', /page-2/);
-      cy.contains('main > h1', 'Hi from the second page');
-      cy.contains('a', 'Go back to the homepage').click();
-      cy.url().should('include', Cypress.config('baseUrl'));
-    })
-  );
+  it('navigates to second page and back', () => {
+    cy.contains('a', 'Go to page 2').click();
+    cy.url().should('match', /page-2/);
+    cy.contains('main > h1', 'Hi from the second page');
+    cy.contains('a', 'Go back to the homepage').click();
+    cy.url().should('include', Cypress.config('baseUrl'));
+  });
 });
